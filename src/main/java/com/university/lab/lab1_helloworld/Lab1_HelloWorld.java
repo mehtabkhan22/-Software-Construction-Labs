096033e2458c5a0fca101b842ceb782ee3ffacee
mehtabkhan22 <khaniaslam3429@gmail.com>
2026-08-20 01:11:45 +0500

    Initial Lab 1 commit

---
+++ src/main/java/com/university/lab/lab1_helloworld/Lab1_HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.university.lab.lab1_helloworld;
+
+/**
+ *
+ * @author Rana
+ */
+public class Lab1_HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello, Software Construction!");
+        System.out.println("My Java Environment is successfully configured.");
+        
+    }
+}
